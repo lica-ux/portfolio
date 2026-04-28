0a08ac0166508bde0e194a7a993e0df082732106
lica-ux <lisa@caspersson.biz>
2026-04-28 20:06:04 +0200

    chore: auto-save 2026-04-28 20:06

--- src/components/MoreWork/MoreWork.tsx
+++ src/components/MoreWork/MoreWork.tsx
@@ -79,7 +79,7 @@ export default function MoreWork() {
         />
       </div>
 
-      <div className="relative flex flex-col gap-12">
+      <div className="relative flex flex-col gap-20">
         {/* Heading */}
         <div className="px-4 md:px-10">
           <h2
