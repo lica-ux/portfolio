c8725e0019c9295807699f889362e7225ab68003
lica-ux <lisa@caspersson.biz>
2026-04-28 20:05:21 +0200

    chore: auto-save 2026-04-28 20:05

--- src/components/MoreWork/MoreWork.tsx
+++ src/components/MoreWork/MoreWork.tsx
@@ -91,7 +91,7 @@ export default function MoreWork() {
         </div>
 
         {/* Project grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10 gap-x-8 px-4 md:px-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-16 gap-x-8 px-4 md:px-10">
           {PROJECTS.map((project) => (
             <article
               key={project.title}
