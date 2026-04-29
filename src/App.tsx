db850834b5adead17ca5c353a1f52dca98de5181
lica-ux <lisa@caspersson.biz>
2026-04-29 11:01:19 +0200

    feat: register /work/* routes for case pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,20 @@ import Home from './pages/Home'
 import Components from './pages/Components'
 import Tokens from './pages/Tokens'
 import About from './pages/About'
+import CasePage from './pages/CasePage'
 
 function App() {
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/"           element={<Home />} />
-        <Route path="/components" element={<Components />} />
-        <Route path="/tokens"     element={<Tokens />} />
-        <Route path="/about"      element={<About />} />
+        <Route path="/"                  element={<Home />} />
+        <Route path="/components"        element={<Components />} />
+        <Route path="/tokens"            element={<Tokens />} />
+        <Route path="/about"             element={<About />} />
+        <Route path="/work/baribuddy"    element={<CasePage slug="baribuddy" />} />
+        <Route path="/work/booky"        element={<CasePage slug="booky" />} />
+        <Route path="/work/sejfa"        element={<CasePage slug="sejfa" />} />
       </Routes>
     </BrowserRouter>
   )
