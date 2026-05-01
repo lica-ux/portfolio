cebb3160a1d240651f71c55d0c8f9a2f53fc91c1
lica-ux <lisa@caspersson.biz>
2026-05-01 15:06:53 +0200

    chore: auto-save 2026-05-01 15:06

--- src/components/CaseHero/CaseHero.tsx
+++ src/components/CaseHero/CaseHero.tsx
@@ -4,7 +4,7 @@ import RevealImage from '../RevealImage'
 // TODO: replace imageSrc with local .webp (max 2080×2080px, max 940kb)
 export default function CaseHero({ imageSrc, imageAlt }: CaseHeroProps) {
   return (
-    <div className="w-full overflow-hidden rounded-[2px] h-[240px] md:h-[640px]">
+    <div className="w-full overflow-hidden rounded-[2px] aspect-[4/3] md:aspect-auto md:h-[640px]">
       <RevealImage
         src={imageSrc}
         alt={imageAlt}
