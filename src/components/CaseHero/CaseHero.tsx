6674ab9bfe70192f15f9e6c930cb70c61c15aec2
lica-ux <lisa@caspersson.biz>
2026-05-01 14:34:17 +0200

    feat: reveal animation on CaseHero image

--- src/components/CaseHero/CaseHero.tsx
+++ src/components/CaseHero/CaseHero.tsx
@@ -1,10 +1,11 @@
 import type { CaseHeroProps } from './CaseHero.types'
+import RevealImage from '../RevealImage'
 
 // TODO: replace imageSrc with local .webp (max 2080×2080px, max 940kb)
 export default function CaseHero({ imageSrc, imageAlt }: CaseHeroProps) {
   return (
     <div className="w-full overflow-hidden rounded-[2px] h-[240px] md:h-[640px]">
-      <img
+      <RevealImage
         src={imageSrc}
         alt={imageAlt}
         className="w-full h-full object-cover"
