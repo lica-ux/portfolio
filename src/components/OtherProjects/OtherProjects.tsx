f6dcef0a698ea3c56cf14fdaf68bf02fc78e3e7f
lica-ux <lisa@caspersson.biz>
2026-05-01 19:02:29 +0200

    chore: auto-save 2026-05-01 19:02

--- src/components/OtherProjects/OtherProjects.tsx
+++ src/components/OtherProjects/OtherProjects.tsx
@@ -40,7 +40,7 @@ export default function OtherProjects({ currentSlug }: OtherProjectsProps) {
       className="flex flex-col md:flex-row gap-8 -mx-4 md:-mx-10 px-4 md:px-10 pt-8 pb-[104px]"
     >
       <h2
-        className="flex-1 basis-0 min-w-[200px] text-[28px] md:text-[40px] lg:text-[72px] font-medium leading-[1.04]"
+        className="flex-1 basis-0 min-w-[200px] text-[40px] lg:text-[72px] font-medium leading-[1.04]"
         style={{ color: 'var(--color-nav-text)', fontFamily: 'var(--font-display)' }}
       >
         Other projects
